c548df79731d7848f2d287f59749ba69bb41593c
Spirits <13>
2022-09-01 15:09:31 +0800

    1.01

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,12 @@ module com.example.demo {
         opens com.example.demo to javafx.fxml;
         opens com.example.demo.register to javafx.fxml;
         opens com.example.demo.menu to javafx.fxml;
+        opens com.example.demo.permission to javafx.fxml;
 
         exports com.example.demo.register;
         exports com.example.demo.homepage;
         exports com.example.demo.menu;
+        exports com.example.demo.permission;
 
 
 
