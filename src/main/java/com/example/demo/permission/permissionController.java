b5355e58d1fc14b77618a7b88d14b104552201a0
Spirits <13>
2022-09-01 15:11:25 +0800

    1.02

---
+++ src/main/java/com/example/demo/permission/permissionController.java
@@ -0,0 +1,15 @@
+package com.example.demo.permission;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class permissionController {
+    @FXML
+    private Button roleManagement;
+
+    @FXML
+    private Button accountManagement;
+
+    @FXML
+    private Button permissionManagement;
+}
