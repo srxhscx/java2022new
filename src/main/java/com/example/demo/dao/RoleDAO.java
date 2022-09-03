1288214a10b7bdbd7f92d60f8cacf898cf9949bb
乱世繁华 <1414896192@qq.com>
2022-09-03 12:07:17 +0800

    1.3

--- src/main/java/com/example/demo/dao/RoleDAO.java
+++ src/main/java/com/example/demo/dao/RoleDAO.java
@@ -1,21 +1,22 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Role;
 import com.example.demo.util.ConnectionUtil;
-import com.example.demo.vo.Role;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RoleDAO {
-
+    /*
+     *
+     * 添加一个角色
+     */
 
     public int delRole(int id){
         int m = -1;
-        String sql = "delete from role_info where role_id = ?";
+        String sql = "delete from user_info where user_id = ?";
         Connection cn = ConnectionUtil.getConnection();
 
         PreparedStatement pstmt =  null;
@@ -41,7 +42,7 @@ public class RoleDAO {
 
     public int updateRole(int id){
         int m = -1;
-        String sql = "UPDATE role_info SET role_name = '店长',role_permission = '修改' WHERE role_id = ?;";
+        String sql = "UPDATE user_info SET user_account = '店长' WHERE user_id = ?;";
         Connection cn = ConnectionUtil.getConnection();
 
         PreparedStatement pstmt =  null;
@@ -64,11 +65,10 @@ public class RoleDAO {
         }
         return m;
     }
-
     public int addRole(String roleName,String rolePermission) throws SQLException {
         //获取连接
         Connection cn = ConnectionUtil.getConnection();
-        String sql = "insert into role_info(role_name,role_permission)values('"+roleName+"','"+rolePermission+"')";
+        String sql = "insert into user_info(user_id,user_account,user_password,locked,face_img,role_id)values("+roleName+"+"+rolePermission+")";
         //发送给mysql执行
         PreparedStatement pstmt =  null;
 
@@ -93,28 +93,21 @@ public class RoleDAO {
         return n;
     }
 
-    public Role getRolesById(int id){
-        int n = -1;
-        String sql = "select role_name,role_permission from role_info where role_id = ?";
+    public Role getRoleById(int id){
         Connection cn = ConnectionUtil.getConnection();
-
+        String sql = "SELECT user_id,user_account FROM user_info WHERE user_id=?";
+        //发送给mysql执行
         PreparedStatement pstmt =  null;
-
-       //声明结果集游标，用于查询每一行数据
-        ResultSet rs = null;
-
+        ResultSet rs=null;
         try{
             pstmt = cn.prepareStatement(sql);
             //返回影响行数
-           // n = pstmt.executeUpdate();
             pstmt.setInt(1,id);
-            rs = pstmt.executeQuery();
-            //判断查询结果集是否有数据
+            rs=pstmt.executeQuery();
             if(rs.next()){
-                Role role = new Role();
-                //使用rs.get...(查询结果集)
-                role.setRole_name(rs.getString("role_name"));
-                role.setRole_permission(rs.getString("role_permission"));
+                Role role =new Role();
+                role.setUser_id(rs.getInt("user_id"));
+                role.setUser_account(rs.getString("user_account"));
                 return role;
             }
         }catch(SQLException e) {
@@ -129,76 +122,14 @@ public class RoleDAO {
             }
         }
         return null;
-
     }
-
-
-    //查询所有数据
-    public List<Role> getRoles(){
-        int n = -1;
-        String sql = "select role_name,role_permission from role_info";
-        Connection cn = ConnectionUtil.getConnection();
-
-        PreparedStatement pstmt =  null;
-
-        //声明结果集游标，用于查询每一行数据
-        ResultSet rs = null;
-
-        //创建一个集合，用于存储多条数据
-        List<Role> roleList = new ArrayList<>();
-
-        try{
-            pstmt = cn.prepareStatement(sql);
-            //返回影响行数
-
-            rs = pstmt.executeQuery();
-            //判断查询结果集是否有数据
-            while(rs.next()){
-                //每一行对象存储到role中
-                Role role = new Role();
-                //使用rs.get...(查询结果集)
-                role.setRole_name(rs.getString("role_name"));
-                role.setRole_permission(rs.getString("role_permission"));
-                //再存储到roleList中
-                roleList.add(role);
-
-            }
-        }catch(SQLException e) {
-            e.printStackTrace();
-        }finally {
-            if (cn != null){
-                try {
-                    cn.close();
-                }catch (SQLException e){
-                    e.printStackTrace();
-                }
-            }
-        }
-        return roleList;
-
-    }
-
-
-
-
-
     public static void main(String[] args) throws SQLException {
         RoleDAO roleDAO = new RoleDAO();
-        //int m = roleDAO.addRole("超级管理员","修改");
-        //System.out.println(m);
-        //int m = roleDAO.delRole(3);
-        //System.out.println(m);
-        //int m = roleDAO.updateRole(2);
-        //System.out.println(m);
-        //Role role = roleDAO.getRolesById(1);
-        //System.out.println(role.getRole_name() + role.getRole_permission()  );
+//        int m = roleDAO.updateRole(2);
+//        System.out.println(m);
+        Role role =roleDAO.getRoleById(2);
+        System.out.println(role.getUser_id()+ role.getUser_account());
 
-        List<Role> list  = roleDAO.getRoles();
-        //System.out.println(list);
-        list.forEach(role -> {
-            System.out.println(role.getRole_name()+","+role.getRole_permission());
-        });
-        //list.forEach(System.out::println);
 
     }
 }
--- src/main/java/com/example/demo/register/RegiterController.java
+++ src/main/java/com/example/demo/register/RegiterController.java
@@ -6,16 +6,19 @@ import com.example.demo.dao.UserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javax.print.DocFlavor;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class RegiterController {
+public class RegiterController implements Initializable {
 
         @FXML
         private Button regiterBtn;
@@ -33,46 +36,49 @@ public class RegiterController {
         private TextField nameText;
 
         @FXML
-       // private ComboBox permission = new ComboBox();
-        private ComboBox<String> permission = new ComboBox<>();
+        private ComboBox permission;
 
-
-
-
-
-
+        @FXML
+        private Label passLabel;
         @FXML
         void regiter(ActionEvent event) throws SQLException {
                 String tel = this.idText.getText();
                 String password = this.passText.getText();
+                String password1 = this.pass1Text.getText();
                 String userName = this.nameText.getText();
 
+                String roleId = permission.getValue().toString();
+                int role_id = roleId.charAt(0) - 48;
+                System.out.println(role_id);
+
+                if (password.equals(password1)){
+                        UserDAO userDAO = new UserDAO();
+                        userDAO.addRole(tel,password,userName,role_id);
+
+                }
+                else{
+                        passLabel.setText("两次密码不一致");
+                }
+
+        }
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
                 permission.getItems().addAll(
                         "1(管理员，修改)",
                         "2(店长，控制)",
                         "3(客户，只读)"
                 );
-//数据源为空时显示
+                //数据源为空时显示
                 permission.setPlaceholder(new Label("Placeholder"));
-//设置可编辑
+                //设置可编辑
                 permission.setEditable(true);
                 permission.setPromptText("PromptText");
-//设置可见行数, 超过显示滚动条
+                //设置可见行数, 超过显示滚动条
                 permission.setVisibleRowCount(3);
 
 
-
-                permission.setValue("Normal");
-
-                //MainRegister mainRegister = new MainRegister();
-                //String roleId = mainRegister.comboBox.getValue().toString();
-                //int role_id = roleId.charAt(0);
-
-                UserDAO userDAO = new UserDAO();
-//                userDAO.addRole(tel,password,userName,role_id);
-
         }
-
 }
 
 
