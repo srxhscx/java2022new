78fef8539216b76f2057d11a14912546c41f6d60
Spirits <13>
2022-09-03 13:43:45 +0800

    1.4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,10 +10,14 @@ module com.example.demo {
         opens com.example.demo to javafx.fxml;
         opens com.example.demo.register to javafx.fxml;
         opens com.example.demo.menu to javafx.fxml;
+        opens com.example.demo.accountManagement to javafx.fxml;
+        opens com.example.demo.roleManagement to javafx.fxml;
 
         exports com.example.demo.register;
         exports com.example.demo.homepage;
         exports com.example.demo.menu;
+        exports com.example.demo.accountManagement;
+        exports com.example.demo.roleManagement;
 
 
         }
